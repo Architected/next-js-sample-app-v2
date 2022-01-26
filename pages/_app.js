d2aab2490e4172a1185bd3e57e1aa9de1f7aa76d
Christian Farinella <christian@architected.net>
2022-01-27 06:30:15 +1100

    Project plumbing

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
+
+// /homeScreen
+
+// /shared/sidePanel
+
+// /layout/authContainer
+// state
+// side panel
+// page title
+
+// /layout/contentContainer
+
+// /layout/footer
+
+// /layout/leftNav
+
+// /latout/topNav
